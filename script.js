e95ff1c6fe713097bc840ec98a9e26114d6982e5
scoz <coz.steven@gmail.com>
2025-12-10 12:09:10 +0100

    update for day 10

--- script.js
+++ script.js
@@ -1,53 +1,76 @@
-const door = document.getElementById("door");
-const riddle = document.getElementById("riddle");
-const error = document.getElementById("error");
-const video = document.getElementById("video");
-const confettis = document.getElementById("confettis");
-//const photo = document.querySelector(".photo");
 
 // Ouvre la porte
 door.addEventListener("click", () => {
   door.classList.add("open");
 });
 
-function blindtest() {
-  riddle.style.display = "none";
-  video.classList.remove("hidden");
-}
+const captions = [
+  "En avant pour l'aventure !", // 0
+  "Bonjour Thaïlande !", // 1
+  "Souvenirs d'une soirée endiablée", // 2
+  "The banana resort et ses couchers de soleil", // 3
+  "♥️", // 4
+  "Miaaam", // 5
+  "Une star à mes yeux", // 6
+  "Plongééée", // 7
+  "Bangkok et ses temples", // 8
+  "Gros bécot", // 9
+  "Mon dieu ma tête", // 10
+  "ça valait le réveil", // 11
+  "Ahahaha", // 12
+  "Une vie de rooots", // 13
+  "Que bellaaaaa", // 14
+  "Encore 2 mois et demi avec toi ?", // 15
+  "Laaaaaos, nous voilà", // 16
+  "Vie de bohème", // 17
+  "Passion juiiiice", // 18
+  "Jolie photo", // 19
+  "Y'a des trucs qui changent pas", // 20
+  "Vite je veux mangeeeeer", // 21
+  "En bonne compagnie", // 22
+  "Barbapapaaaaa", // 23
+  "Good morning Vietnaaaam", // 24
+  "Gorgeous", // 25
+  "Elle est trop bien la photo aha", // 26
+  "Incognito", // 27
+  "Trop belle", // 28
+  "Chaudière", // 29
+  "Office with a view", // 30
+  "Bonjour à tous les baboss, on se fait une salutation au soleil?", // 31
+  "Amoureux alcoolisés", // 32
+  "Duuuune", // 33
+  "Influenceuse", // 34
+  "Tu veux une couille ?", // 35
+  "No comment..", // 36
+  "Assise comme la dame de pierre de Brélès", // 37
+  "On y retouuuurne ?", // 38
+  "Lookin for a target", // 39
+];
 
-document.getElementById("validate").addEventListener("click", () => {
-  const val = document.getElementById("answer").value.trim().toLowerCase();
+const images = Array.from({ length: 44 }, (_, i) => `assets/photo${i + 1}.jpg`);
 
-  if (val === 'Décathlon' || val === 'Decathlon' || val === 'décathlon' || val === 'decathlon') {
-    launchConfetti();
-  }
+
+let index = 0;
+const imgElement = document.getElementById("galleryImage");
+const caption = document.getElementById("caption");
+
+document.getElementById("nextBtn").addEventListener("click", () => {
+  index = (index + 1) % images.length;
+  updateImage();
 });
 
+document.getElementById("prevBtn").addEventListener("click", () => {
+  index = (index - 1 + images.length) % images.length;
+  updateImage();
+});
 
-
-function launchConfetti() {
-  const confettiContainer = document.getElementById("confettis");
-
-  for (let i = 0; i < 80; i++) {
-
-    const confetti = document.createElement("div");
-    confetti.classList.add("confetti-piece");
-
-    // Position de départ aléatoire
-    confetti.style.left = Math.random() * 100 + "%";
-
-    // Couleur aléatoire
-    const colors = ["#e74c3c", "#f1c40f", "#2ecc71", "#3498db", "#9b59b6"];
-    confetti.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
-
-    // Durée de la chute
-    confetti.style.animationDuration = 2 + Math.random() * 2 + "s";
-
-    confettiContainer.appendChild(confetti);
-
-    // Suppression après animation
-    setTimeout(() => confetti.remove(), 4000);
-  }
+function updateImage() {
+  imgElement.style.opacity = 0;
+  setTimeout(() => {
+    imgElement.src = images[index];
+    imgElement.style.opacity = 1;
+    caption.textContent = captions[index];
+  }, 200);
 }
 
 
